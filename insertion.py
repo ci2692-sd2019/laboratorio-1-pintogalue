35a293da87abe712e18f724ccb61a345220bb613
Joao Pinto <joaobose@gmail.com>
2019-09-19 13:54:54 -0400

    implementacion de insertion sort y test

--- insertion.py
+++ insertion.py
@@ -1,4 +1,13 @@
-
-
 def insertion_sort(A):
-    pass
+    x = 0
+    j = 0
+
+    for i in range(1,len(A)):
+        x = A[i]
+        j = i - 1
+
+        while j >= 0 and x < A[j]:
+            A[j + 1] = A[j]
+            j -= 1
+
+        A[j + 1] = x
--- test_insertion.py
+++ test_insertion.py
@@ -1,1 +1,13 @@
 from insertion import *
+
+R = []
+filename = './test.txt'
+
+with open(filename,'r') as file:
+    raw = file.readlines()
+    R = [int(k) for k in raw]
+    print('secuencia antes de ejecutar insertion sort:')
+    print(R)
+    print('\nsecuencia resultante de aplicar insertion sort')
+    insertion_sort(R)
+    print(R)
